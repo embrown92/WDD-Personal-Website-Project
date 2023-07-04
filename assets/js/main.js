b8745a8c62560cdfbe0c121acbde286e2750237c
Eda Mae Brown <eda_awe92@yahoo.com.ph>
2023-07-04 13:27:30 -0400

    added css and javascript

---
+++ assets/js/main.js
@@ -0,0 +1,115 @@
+/*===== MENU SHOW =====*/
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId)
+
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle', 'nav-menu')
+
+/*===== REMOVE MENU MOBILE =====*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*===== SCROLL SECTIONS ACTIVE LINK =====*/
+const sections = document.querySelectorAll('section[id]')
+
+window.addEventListener('scroll', scrollActive)
+
+function scrollActive() {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
+        } else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
+        }
+    })
+}
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+})
+
+/*SCROLL SECTION NAME*/
+sr.reveal('.section-title', { origin: 'top' })
+
+/*SCROLL PORTFOLIO WORK*/
+
+sr.reveal('.portfolio-work', { origin: 'top', delay: 100 })
+
+
+/*SCROLL HOME*/
+sr.reveal('.home__img', { origin: 'left' })
+sr.reveal('.home__title', { origin: 'right', delay: 100 })
+sr.reveal('.home__subtitle', { origin: 'right', delay: 200 })
+sr.reveal('.home__profession', { origin: 'right', delay: 300 })
+sr.reveal('.home__text', { origin: 'right', delay: 350 })
+sr.reveal('.home__social-icon', { delay: 600, interval: 200 })
+sr.reveal('.button-box', { interval: 600, origin: 'right' })
+
+
+
+/*SCROLL ABOUT*/
+sr.reveal('.about__img', { delay: 500 })
+sr.reveal('.about__subtitle', { delay: 300 })
+sr.reveal('.about__profession', { delay: 400 })
+sr.reveal('.about__text', { delay: 500 })
+sr.reveal('.about__social-icon', { delay: 600, interval: 200 })
+
+/*SCROLL SKILLS*/
+sr.reveal('.skills__subtitle', {})
+sr.reveal('.skills__name', { distance: '20px', delay: 50, interval: 100 })
+sr.reveal('.skills__img', { delay: 400 })
+
+/*SCROLL PORTFOLIO*/
+sr.reveal('.portfolio__img', { interval: 200 })
+
+/*SCROLL CONTACT*/
+sr.reveal('.contact__subtitle', {})
+sr.reveal('.contact__text', { interval: 200 })
+sr.reveal('.contact__input', { delay: 400 })
+sr.reveal('.contact__button', { delay: 600 })
+
+/*MAIN PORTFOLIO SLIDESHOW*/
+
+sr.reveal('.slideshow', { interval: 200, origin: 'top' })
+sr.reveal('.p-button-box', { interval: 400, origin: 'top' })
+
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    if (n > x.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = x.length };
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex - 1].style.display = "block";
+}
+
